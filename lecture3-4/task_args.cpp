4414ba390851990aa0dd71c8de7e8bbc8efee096
Tsarkov Maksim <mtsarkov@naumen.ru>
2023-09-12 14:33:55 +0300

    [fix] task args file

--- lecture3-4/task_args.cpp
+++ lecture3-4/task_args.cpp
@@ -3,11 +3,10 @@
 
 int main(int argc, char** argv)
 {
-	int i[][2] = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-	int* j = (int*) i;
-	using matrix = int(*)[2];
-	matrix k = (matrix) i;
-	// ??
-	std::cout << j[6] << "=> " << k[0]<< std::endl;
+	std::cout << "command line arguments are:" << std::endl;
+	for (int i = 0; i < argc; ++i)
+	{
+		std::cout << "    " << i << " -> " << argv[i] << std::endl;
+	}
 	return 0;
 }
