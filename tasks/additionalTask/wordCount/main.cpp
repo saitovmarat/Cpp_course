e3fc661f2e2fec4d8a70430ea472dfa48b337d1f
Marat Saitov <maratsajtov@gmail.com>
2023-12-01 14:30:14 +0300

    final

--- tasks/additionalTask/wordCount/main.cpp
+++ tasks/additionalTask/wordCount/main.cpp
@@ -24,10 +24,100 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <memory>
 
+
+int areThereUniqueWords(std::map<std::string, int> uniqueWords){
+    for(std::map<std::string, int>::iterator it = uniqueWords.begin(); it != uniqueWords.end(); it++){
+        if(it->second > 1)
+            return 1;
+    }
+    return 0;
+}
+std::map<std::string, int> uniqueWordsMap(std::vector<std::string> wordsInSentence)
+{
+    std::map<std::string, int> uniqueWords;
+    std::vector<std::string>::iterator it = wordsInSentence.begin();
+    while(it!=wordsInSentence.end()){
+        if (uniqueWords.find(*it) == uniqueWords.end()){
+            uniqueWords[*it] = 1;
+        }
+        else{
+            uniqueWords[*it]++;
+        }
+        it++;
+    }
+    return uniqueWords;
+} 
+std::vector<std::string> Split(const std::string str)
+{
+    std::vector<std::string> result;
+    std::string addString;
+    int i = 0;
+    while (i < str.size()) {
+        if (str[i] != ' ' && str[i] != ',') {
+            addString += str[i]; 
+        } else {
+            if (!addString.empty()) {
+                result.push_back(addString);
+                addString.clear();
+            }
+        }
+        i++;
+    }
+    if (!addString.empty()) {
+        result.push_back(addString);
+    }
+    return result;
+}
+void printResults(std::vector<std::string> words){
+    std::vector<std::string>::iterator wordsIt = words.begin();
+    std::vector<std::string> wordsInSentence;
+    int sentencesCount = 0;
+    while(wordsIt!=words.end()){
+        size_t found = wordsIt->find('.');
+        if (found != std::string::npos){
+            wordsIt->pop_back();
+            std::transform(wordsIt->begin(), wordsIt->end(), wordsIt->begin(), ::tolower);
+            wordsInSentence.push_back(*wordsIt);
+            std::map<std::string, int> uniqueWords = uniqueWordsMap(wordsInSentence);
+            if(areThereUniqueWords(uniqueWords)){
+                std::cout << sentencesCount << ':';
+                for(std::map<std::string, int>::iterator it = uniqueWords.begin(); it != uniqueWords.end(); it++){
+                    if(it->second > 1){
+                        std::cout << it->first << ',';
+                    }
+                }
+                std::cout << "\b \n";
+            }
+            sentencesCount++;
+            wordsInSentence.clear();
+        }
+        else{
+            std::transform(wordsIt->begin(), wordsIt->end(), wordsIt->begin(), ::tolower);
+            wordsInSentence.push_back(*wordsIt);
+        }
+        wordsIt++;
+    }
+}
 
 int main() {
-    // ваше решение
+    std::ifstream inputFile("input.txt");
 
+    if (inputFile.is_open()) {
+        std::vector<std::string> words;
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            std::vector<std::string> wordsInLine = Split(line);
+            for(int i = 0; i < wordsInLine.size(); i++){
+                words.push_back(wordsInLine[i]);
+            }
+        }
+        inputFile.close();
+        printResults(words);
+    } 
+    else{
+        std::cout << "Не удалось открыть файл.\n";
+    }
     return 0;
 }
